58965fe958e1a1e97f6352ca953ef5ae1e49edf6
Tuomas Välimäki <t.valimaki@gmail.com>
2021-01-10 18:11:06 +0200

    Remove useless env variables

--- utils/config.js
+++ utils/config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 let PORT = process.env.PORT || 3000
 
-let REDIS_HOST = process.env.REDIS_HOST
+
 let REDIS_URL = process.env.REDIS_URL
-let REDIS_PORT = process.env.REDIS_PORT || 6379
+
 
 
 const STOPS = {
@@ -16,5 +16,5 @@ const STOPS = {
 const HSL_API_URL = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
 
 module.exports = {
-  STOPS, PORT, HSL_API_URL, REDIS_HOST, REDIS_PORT, REDIS_URL
+  STOPS, PORT, HSL_API_URL, REDIS_URL
 }
