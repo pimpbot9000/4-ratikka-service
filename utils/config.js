33939e9511783bd699db9fc39737846e4191d9e0
Tuomas Välimäki <t.valimaki@gmail.com>
2021-03-15 18:57:31 +0200

    Add kadettikoulu stop

--- utils/config.js
+++ utils/config.js
@@ -2,15 +2,13 @@ require('dotenv').config()
 
 let PORT = process.env.PORT || 3000
 
-
 let REDIS_URL = process.env.REDIS_URL
-
-
 
 const STOPS = {
   portti: 1301451,
   alepa: 1301453,
-  paattari: 1301455
+  kadetti: 1301455,
+  paattari: 1301456 
 }
 
 const HSL_API_URL = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
