e605d39cda787c59fdd8eccf7a9834576fd13c57
pimpbot9000 <t.valimaki@gmail.com>
2021-03-16 01:23:19 +0200

    Add redis as middleware: now it will treat different endpoints separately

--- controllers/timetable.js
+++ controllers/timetable.js
@@ -7,10 +7,17 @@ timetableRouter.get('/', (request, response) => {
   return response.status(200).send('Vaihtoehtoina ovat: /portti, /alepa, /kadetti ja /paattari').end()
 })
 
-timetableRouter.get('/:id', cache.route({ expire: 5 }), async (request, response) => {
-
+timetableRouter.get('/:id', (request, response, next) => {
+  response.express_redis_cache_name = `stop-${request.params.userid}`  
+  next()
+  },  
+  
+  cache.route({expire: 5}), 
+  
+  async (request, response) => {
+  
   const id = request.params.id
-
+  
   const stopId = config.STOPS[id]
 
   if (!stopId) return response.status(404).send('404: Pysäkkiä ei löydy. Vain munccalaisille. Köyhä.').end()
@@ -46,15 +53,7 @@ timetableRouter.get('/:id', cache.route({ expire: 5 }), async (request, response
 
     const arrivals = result.data.data.stop.stoptimesWithoutPatterns
 
-    const arrivalTimes = arrivals.map(time => {
-      return {
-        departureInMinutes: calculateMinutes(time),
-        departureInSeconds: calculateSeconds(time),
-        description: time.headsign,
-        sign: time.trip.tripHeadsign,
-        route: time.trip.routeShortName
-      }
-    }).filter(t => t.departureInSeconds >= 0)
+    const arrivalTimes = getArrivalTimes(arrivals)
 
     response
       .status(200)
@@ -66,6 +65,23 @@ timetableRouter.get('/:id', cache.route({ expire: 5 }), async (request, response
       .send('Server error').end()
   }
 })
+
+const getArrivalTimes = (arrivals) => {
+  arrivalTimes = arrivals.map(time => {
+    return {
+      departureInMinutes: calculateMinutes(time),
+      departureInSeconds: calculateSeconds(time),
+      description: time.headsign,
+      sign: time.trip.tripHeadsign,
+      route: time.trip.routeShortName
+    }
+  }).filter(t => t.departureInSeconds >= 0)
+
+  return arrivalTimes
+
+}
+
+
 
 const calculateMinutes = (time) => {
   const timeInMinutes = calculateSeconds(time) / 60
