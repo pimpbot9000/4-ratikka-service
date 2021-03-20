e430b5beec5f9363ceece1c6776233bf65c18b35
pimpbot9000 <t.valimaki@gmail.com>
2021-03-20 14:37:51 +0200

    Serve static web version for arabianranta

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const timetableRouter = require('./controllers/timetable')
 const middleware = require('./utils/middleware')
 const app = express()
 
+
+app.use('/web', express.static('static'))
 app.use('/api', timetableRouter)
 app.use(middleware.unknownEndpoint)
 
