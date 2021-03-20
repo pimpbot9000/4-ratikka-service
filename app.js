8d7a4991e2873eda19413cedefdc4bcf155f2695
pimpbot9000 <t.valimaki@gmail.com>
2021-03-20 15:20:54 +0200

    user cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const timetableRouter = require('./controllers/timetable')
 const middleware = require('./utils/middleware')
+const cors =require('cors')
 const app = express()
 
-
+app.use(cors());
 app.use('/web', express.static('static'))
 app.use('/api', timetableRouter)
 app.use(middleware.unknownEndpoint)
